fa7ec3e9a2c8edd063c7c478b39682ee7c415bb9
crok <crok.bic@gmail.com>
2021-02-15 10:55:43 +0100

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,8 @@
+ui_print ""
+ui_print "################################"
+ui_print "#                              #"
+ui_print "# crok's RAM management tweaks #"
+ui_print "#                              #"
+ui_print "################################"
+ui_print ""
+
---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+
+# Delete Activity Manager's max. cached app number variable and system will use the Activity Manager's default
+settings delete global activity_manager_constants
+

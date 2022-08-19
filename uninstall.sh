7dacd57bd7163fa82a09af90330bd73f2df59c39
crok <crok.bic@gmail.com>
2022-08-20 00:51:57 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 
 # Delete Activity Manager's max. cached app number variable and system will use the Activity Manager's default settings:
-[ $(getprop ro.build.version.release) -gt 9 ] && /system/bin/device_config reset trusted_defaults activity_manager ; /system/bin/device_config put activity_manager max_phantom_processes default ; /system/bin/device_config put activity_manager max_cached_processes default || settings delete global activity_manager_constants
+[ $(getprop ro.build.version.release) -gt 9 ] && /system/bin/device_config put activity_manager max_phantom_processes default ; /system/bin/device_config put activity_manager max_cached_processes default || settings delete global activity_manager_constants

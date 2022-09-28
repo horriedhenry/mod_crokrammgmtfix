cf7b16a76ed8621de9e35c3a0a081d07b1a48af3
crok <crok.bic@gmail.com>
2022-09-28 08:51:48 +0200

    Upgrade to 0.0.8.1

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,11 @@
 #!/system/bin/sh
 
-# Delete Activity Manager's max. cached app number variable and system will use the Activity Manager's default settings:
-[ $(getprop ro.build.version.release) -gt 9 ] && /system/bin/device_config put activity_manager max_phantom_processes default ; /system/bin/device_config put activity_manager max_cached_processes default || settings delete global activity_manager_constants
+# Delete / reset Activity Manager's variables set by the module and system will use the Activity Manager's default settings after a restart:
+[ $(getprop ro.build.version.release) -gt 9 ] && cmd device_config set_sync_disabled_for_tests none
+[ $(getprop ro.build.version.release) -gt 9 ] && cmd device_config delete activity_manager max_cached_processes || settings delete global activity_manager_constants
+[ $(getprop ro.build.version.release) -gt 9 ] && cmd device_config delete activity_manager max_phantom_processes
+[ $(getprop ro.build.version.release) -gt 9 ] && cmd settings reset global settings_enable_monitor_phantom_procs
+[ $(getprop ro.build.version.release) -gt 9 ] && cmd device_config delete activity_manager max_empty_time_millis
+[ $(getprop ro.build.version.release) -gt 9 ] && cmd settings delete global settings_enable_monitor_phantom_procs
+
+echo "Please restart the device to make sure RescueParty and RollbackManager restore the default settings."

55c317c519e0560a6cbc9f9992aef3085c0d70c2
Shiva <105149843+horriedhenry@users.noreply.github.com>
2024-08-17 20:38:59 +0530

    Update service.sh

--- service.sh
+++ service.sh
@@ -25,7 +25,7 @@
 sleep 180
 
 [ $(getprop ro.build.version.release) -gt 9 ] && cmd device_config set_sync_disabled_for_tests persistent
-[ $(getprop ro.build.version.release) -gt 9 ] && cmd device_config put activity_manager max_cached_processes 256 || settings put global activity_manager_constants max_cached_processes=256
+[ $(getprop ro.build.version.release) -gt 9 ] && cmd device_config put activity_manager max_cached_processes 160 || settings put global activity_manager_constants max_cached_processes=160
 [ $(getprop ro.build.version.release) -gt 9 ] && cmd device_config put activity_manager max_phantom_processes 2147483647
 [ $(getprop ro.build.version.release) -gt 9 ] && cmd settings put global settings_enable_monitor_phantom_procs false
 [ $(getprop ro.build.version.release) -gt 9 ] && cmd device_config put activity_manager max_empty_time_millis 43200000
